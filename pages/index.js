08a706d9aa1fae5bfa7a9b2dad0c302466ebdc8f
KevProDev <kevintheleader@yahoo.com>
2021-10-27 18:33:18 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,8 @@ export default function Home() {
       <header>
         <nav className="container flex items-center py-4 mt-4 sm:mt-12">
           <div className="py-1">
-            <img src="/logo-bookmark.svg" alt="" />
+            {/* <img src="/logo-bookmark.svg" alt="" /> */}
+            <h1 className="font-bold text-2xl">KEVIN JOHNSON JR</h1>
           </div>
           <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-bookmark-blue uppercase text-xs">
             <li className="cursor-pointer">About </li>
@@ -31,14 +32,13 @@ export default function Home() {
           {/* Content */}
           <div className="flex flex-1 flex-col items-center lg:items-start">
             <h2 className="text-bookmark-blue text-3xl md:text-5xl text-center lg:text-left mb-2">
-              Kevin Johnson Jr
+              Creating A Better Future and Loving It!
             </h2>
             <h3 className="text-bookmark-blue text-xl md:text-2xl text-center lg:text-left mb-6">
               Frontend Web Developer
             </h3>
             <p className="text-bookmark-grey text-lg text-center lg:text-left mb-6">
-              React is where I spend my time coding, but I'm always open to
-              learn new technologies
+              Hi, I'm Kevin Johnson
             </p>
             <div className="flex justify-center flex-wrap gap-6">
               <button
@@ -66,22 +66,36 @@ export default function Home() {
         </div>
       </section>
 
+      {/* About Me */}
+      <section className="bg-bookmark-purple py-20 mt-20 lg:mt-60">
+        <div className="sm:w-3/4 lg:w-3/5 mx-auto px-2">
+          <h1 className="text-2xl text-center text-bookmark-white">
+            Hi, I’m Kevin and I'm looking for a team that allows me to
+            perpetually get out of my comfort zone. The MERN stack, NextJs and
+            Wordpress is where I spend bulk of my time learning, but I’m always
+            excited to learn new technologies.
+          </h1>
+        </div>
+      </section>
       {/* Features */}
-      <section className="bg-bookmark-white py-20 mt-20 lg:mt-60">
+      <section className="bg-bookmark-white py-20">
         {/* Heading */}
         <div className="sm:w-3/4 lg:w-5/12 mx-auto px-2">
           <h1 className="text-3xl text-center text-bookmark-blue">
             Recent Work
           </h1>
-          <p className="text-center text-bookmark-grey mt-4">
+          {/* <p className="text-center text-bookmark-grey mt-4">
             Helping ideas come into the world one line of code at a time below.
-          </p>
+          </p> */}
         </div>
         {/* Project #1 */}
         <div className="relative mt-20 lg:mt-24">
           <div className="container flex flex-col lg:flex-row items-center justify-center gap-x-24">
             {/* Image */}
-            <div className="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
+            <div className="flex flex-col flex-1 justify-center z-10 mb-10 lg:mb-0">
+              <h3 className="text-bookmark-blue text-xl md:text-2xl text-center lg:text-left mb-6">
+                Video Demo
+              </h3>
               <img
                 className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full"
                 src="/illustration-features-tab-1.png"
@@ -91,23 +105,127 @@ export default function Home() {
             {/* Content */}
             <div className="flex flex-1 flex-col items-center lg:items-start">
               <h1 className="text-3xl text-bookmark-blue">
-                Know what meals are the best
+                <span className="font-bold">Know </span>What To Pick Eating Out
               </h1>
               <p className="text-bookmark-grey my-4 text-center lg:text-left sm:w-3/4 lg:w-full">
-                See what meals your friends enjoy from resturants all over the
-                city in one central place. Share your favorite meals to help
-                another person make a descision.
+                As living in Chicago we have a large amount of diverse
+                restaurants, when trying out a new restaurants for the first
+                time you don’t have a clue what a meal can may taste like based
+                on the menu sometimes.
+                <br />
+                <br />I created a website where friends can recommend the best
+                meals from a restaurants they have been too. This was built
+                using React, Tailwind for the Frontend. NextJs for speed and
+                Backend. Mongodb for the database and Vercel for deployment.
+                This will allow small restaurants to get discovered from
+                friends.
               </p>
-              <button
-                type="button"
-                className="btn btn-purple hover:bg-bookmark-white hover:text-black"
-              >
-                Foodairy Website
-              </button>
+              <div className="flex justify-center flex-wrap gap-6">
+                <button
+                  type="button"
+                  className="btn btn-purple hover:bg-bookmark-white hover:text-black"
+                >
+                  FoodBuddy Website
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-white hover:bg-bookmark-purple hover:text-white"
+                >
+                  Github
+                </button>
+              </div>
             </div>
           </div>
           {/* Rounded Rectangle */}
           <div className="hidden lg:block overflow-hidden rounded-r-full bg-bookmark-purple absolute h-80 w-2/4 -bottom-24 lg:-left-36  "></div>
+        </div>
+      </section>
+      {/* Features */}
+      <section className="bg-bookmark-white pt-20 pb-40">
+        {/* Heading */}
+        <div className="sm:w-3/4 lg:w-5/12 mx-auto px-2">
+          <h1 className="text-3xl text-center text-bookmark-blue">Blog</h1>
+          {/* <p className="text-center text-bookmark-grey mt-4">
+            Helping ideas come into the world one line of code at a time below.
+          </p> */}
+        </div>
+        {/* Project #1 */}
+        <div className="relative mt-20 lg:mt-24">
+          <div className="container flex flex-row-reverse items-center justify-center gap-x-24 z-10">
+            {/* Image */}
+            <div className="flex flex-col flex-1 justify-center z-10 mb-10 lg:mb-0">
+              <h3 className="text-bookmark-blue text-xl md:text-2xl text-center lg:text-left mb-6">
+                Video Demo
+              </h3>
+              <img
+                className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full"
+                src="/illustration-features-tab-1.png"
+                alt="project one"
+              />
+            </div>
+            {/* Content */}
+            <div className="flex flex-1 flex-col items-center lg:items-start">
+              <h1 className="text-3xl text-bookmark-blue">
+                <span className="font-bold">Know </span>What To Pick Eating Out
+              </h1>
+              <p className="text-bookmark-grey my-4 text-center lg:text-left sm:w-3/4 lg:w-full">
+                As living in Chicago we have a large amount of diverse
+                restaurants, when trying out a new restaurants for the first
+                time you don’t have a clue what a meal can may taste like based
+                on the menu sometimes.
+                <br />
+                <br />I created a website where friends can recommend the best
+                meals from a restaurants they have been too. This was built
+                using React, Tailwind for the Frontend. NextJs for speed and
+                Backend. Mongodb for the database and Vercel for deployment.
+                This will allow small restaurants to get discovered from
+                friends.
+              </p>
+              <div className="flex justify-center flex-wrap gap-6">
+                <button
+                  type="button"
+                  className="btn btn-purple hover:bg-bookmark-white hover:text-black"
+                >
+                  FoodBuddy Website
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-white hover:bg-bookmark-purple hover:text-white"
+                >
+                  Github
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* Rounded Rectangle */}
+          <div className="hidden lg:block overflow-hidden rounded-l-full bg-bookmark-purple absolute h-80 w-2/4 -bottom-24 lg:-right-36 z-0"></div>
+        </div>
+      </section>
+      {/* Contact */}
+      <section className="bg-bookmark-purple py-20">
+        {/* Heading */}
+        <div className="sm:w-3/4 lg:w-5/12 mx-auto px-2">
+          <h1 className="text-3xl text-center text-white">Contact</h1>
+          {/* <p className="text-center text-bookmark-grey mt-4">
+            Helping ideas come into the world one line of code at a time below.
+          </p> */}
+        </div>
+        {/* Project #1 */}
+        <div className="relative sm:w-3/4 lg:w-5/12 mx-auto mt-20 lg:mt-24">
+          <div className="container flex flex-col lg:flex-row items-center justify-center gap-x-24">
+            {/* Image */}
+            <div className="flex flex-col flex-1 justify-center z-10 mb-10 lg:mb-0">
+              <h1 className="text-3xl text-center text-white">
+                <span className="font-bold text-white">So, </span>I Would Love
+                To Talk!
+              </h1>
+              <img
+                className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full"
+                src="/illustration-features-tab-1.png"
+                alt="project one"
+              />
+            </div>
+          </div>
         </div>
       </section>
     </div>
