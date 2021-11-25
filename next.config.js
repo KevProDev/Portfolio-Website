d49a3dc7eb09af9bd303a60878a29c6c9dd8fb1d
KevProDev <kevintheleader@yahoo.com>
2021-11-25 14:51:57 -0600

    reconfigure settings 4 i got this time

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    userIdKey: process.env.NEXT_PUBLIC_YOUR_SERVICE_ID,
+    userIdKey: process.env.NEXT_PUBLIC_YOUR_USER_ID,
     templateIdKey: process.env.NEXT_PUBLIC_YOUR_TEMPLATE_ID,
     serviceIdKey: process.env.NEXT_PUBLIC_YOUR_SERVICE_ID,
   },
