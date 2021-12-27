79b6db7ef00c371f4c8c86d1ce89e95b2801c999
KevProDev <kevintheleader@yahoo.com>
2021-12-27 15:55:24 -0600

    adding header meta data for sharing

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import "tailwindcss/tailwind.css";
 import "./styles/globals.css";
+import head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <head>
+        <meta property="og:title" content="Kevin Johnson Portfilo" />
+        <meta
+          property="og:description"
+          content="Solving Business Problems with Code"
+        />
+        <meta property="og:image" content="https://imgur.com/a/7o5PdJ3" />
+      </head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
